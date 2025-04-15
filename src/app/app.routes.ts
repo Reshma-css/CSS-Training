98bb93828661d97c69ad24734665afe56185859f
reshma <reshma.kolekar@bluebenz.com>
2025-04-15 16:34:43 +0530

    Day-2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { BasicCSSComponent } from './basic-css/basic-css.component';
+import { BackgroundsComponent } from './backgrounds/backgrounds.component';
 
 export const routes: Routes = [
-    {path:'',component:BasicCSSComponent}
+    {path:'',component:BasicCSSComponent},
+    {path:'background',component:BackgroundsComponent},
 ];
